fb089feac785da804ca6b4f3d66a07e8ec781947
Andres-Hincapie-HRZ <164922026+Andres-Hincapie-HRZ@users.noreply.github.com>
2025-02-28 22:27:10 -0500

    Cambio16

--- Conexion/ConexionBD.js
+++ Conexion/ConexionBD.js
@@ -1,2 +1,26 @@
 // Constante para definicion de conexion
-const Conect 
+const sql = require('mssql');
+
+// CONSTANTE PARA CONFIG DE BASE DATA
+const config ={
+    user: 'sa',
+    password: 'root',
+    server: '192.168.1.156',
+    database: 'LAMUNDIAL_LOCAL',
+    options: {
+        encrypt: false,
+        trustServerCertificate: true
+    }
+}
+
+async function obtenerDatos(){
+    try {
+        let pool = await sql.connect(config);
+        let productos = await pool.request().query('SELECT * FROM inventario');
+        return resultado.recordset;
+    } catch (err) {
+        alert.error('Error al conectar al la BD', err);
+    }
+}
+
+module.exports = obtenerDatos;
